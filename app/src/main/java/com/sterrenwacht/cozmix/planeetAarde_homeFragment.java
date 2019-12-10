c86544dd10f1773c7911765ecb01286442366326
Joke <joke_devriese@hotmail.com>
2019-12-10 23:24:34 +0100

    Integratie planeet-fragmenten "home", "algemeen_planeet", en "algemeen_standbeeld" in 1 fragment. Aanpassingen voorlopig in aarde-fragment, to do later: fix duplicaten planeten.

--- app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_homeFragment.java
+++ app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_homeFragment.java
@@ -7,24 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class planeetAarde_homeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_aarde_home, null);
-
-
-
-
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-
-
-
+        return inflater.inflate(R.layout.fragment_aarde_home, container, false);
     }
 }
