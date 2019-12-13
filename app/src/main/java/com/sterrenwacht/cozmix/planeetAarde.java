7c5efddc28fad4d548897bafd82c2e0def22f7ec
Joke <joke_devriese@hotmail.com>
2019-12-14 00:00:17 +0100

    Navigatiebalk onderaan: fix verkeerde marges, fragment-switches bij klikken op navigatiebalk onderaan toegevoegd aan backstack, knoppen info planeet en info standbeeld gewist (gezien deze fragments ook gemerged). FAB (back-button) gewist. Herschikking layout activity planeet: geen onverwachte overlap meer, correcter gebruik items, algemene verbeteringen. Algemene codeverbeteringen (marges, schikking, layouts, ...).

--- app/src/main/java/com/sterrenwacht/cozmix/planeetAarde.java
+++ app/src/main/java/com/sterrenwacht/cozmix/planeetAarde.java
@@ -1,91 +1,58 @@
 package com.sterrenwacht.cozmix;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class planeetAarde extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    String str;
+    private Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planeet_aarde);
 
-        BottomNavigationView navigation = findViewById(R.id.navigationPluto);
-        navigation.setOnNavigationItemSelectedListener(this);
-
-
-        LoadFragment(new planeetAarde_homeFragment());
-
-
-        //transparrant maken van statues en action bar ///////////////////////////////////////////////////////////
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        if (savedInstanceState == null) {
+            fragment = new planeetAarde_homeFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.planet_container, fragment);
+            ft.commit();
         }
 
-        //Terug knop (rechtsboven) //////////////////////////////////////////////////////////////////////////////
-
-        FloatingActionButton btnTerug = (FloatingActionButton)findViewById(R.id.fabIjsdwergen);
-        btnTerug.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
     private boolean LoadFragment(Fragment fragment){
-
         if (fragment != null){
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.planeetAarde_fragment_container, fragment)
+                    .replace(R.id.planet_container, fragment)
+                    .addToBackStack(null)
                     .commit();
-
             return true;
         }
         return false;
     }
 
-    //Navigatie balk voor de verschillende tabs op een planeet pagina /////////////////////////////////////////////
-
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        Fragment fragment = null;
-
-        switch (menuItem.getItemId()){
+        switch (menuItem.getItemId()) {
             case R.id.navigation_home:
                 fragment = new planeetAarde_homeFragment();
                 break;
             case R.id.navigation_zwaarte:
                 fragment = new planeetAarde_gewichtFragment();
                 break;
-            case R.id.navigation_algemeen_plan:
-                fragment = new planeetAarde_algemeenPlanFragment();
-                break;
-            case R.id.navigation_algemeen_stand:
-                fragment = new planeetAarde_algemeenStandFragment();
-                break;
             case R.id.navigation_weetjes:
                 fragment = new planeetAarde_weetjesFragment();
                 break;
         }
-
         return LoadFragment(fragment);
     }
-
-
 }
--- app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_homeFragment.java
+++ app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_homeFragment.java
@@ -12,7 +12,8 @@ public class planeetAarde_homeFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_aarde_home, container, false);
     }
 }
