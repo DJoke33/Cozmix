767472052fc86a5a07bcc8f15e7d8b9d986e1f63
Joke <joke_devriese@hotmail.com>
2019-12-14 01:01:24 +0100

    Herschikking layout fragment weetjes.

--- app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_weetjesFragment.java
+++ app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_weetjesFragment.java
@@ -12,7 +12,8 @@ public class planeetAarde_weetjesFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_aarde_weetjes, null);
 
 
