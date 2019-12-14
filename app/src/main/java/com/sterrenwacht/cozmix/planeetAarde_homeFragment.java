da7f89bffd6bdeb7f8f8f485c698a1ef38c7635d
Joke <joke_devriese@hotmail.com>
2019-12-14 20:06:07 +0100

    Alle resource links van .xml naar .java gebracht (voorlopig enkel home en weetjes aarde). Herstructurering en update naamgeving string resources (voorlopig enkel aarde).

--- app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_homeFragment.java
+++ app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_homeFragment.java
@@ -1,5 +1,6 @@
 package com.sterrenwacht.cozmix;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class planeetAarde_homeFragment extends Fragment {
 
@@ -14,6 +17,32 @@ public class planeetAarde_homeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_aarde_home, container, false);
+
+        // inflate fragment view
+        View view = inflater.inflate(R.layout.fragment_aarde_home, container, false);
+
+        // prep resource management
+        Resources resources = getResources();
+        String packageName = getActivity().getPackageName();
+
+        // set image resources
+        ((ImageView) view.findViewById(R.id.planet_image))
+                .setImageResource(resources.getIdentifier("plan_aarde2","drawable", packageName));
+        ((ImageView) view.findViewById(R.id.planet_probe_image))
+                .setImageResource(resources.getIdentifier("sonde_aarde", "drawable", packageName));
+        ((ImageView) view.findViewById(R.id.planet_statue_image))
+                .setImageResource(resources.getIdentifier("aarde_statue_description", "drawable", packageName));
+
+        // set text resources
+        ((TextView)view.findViewById(R.id.planet_name))
+                .setText(resources.getIdentifier("aarde_name", "string", packageName));
+        ((TextView) view.findViewById(R.id.planet_description))
+                .setText(resources.getIdentifier("aarde_description", "string", packageName));
+        ((TextView) view.findViewById(R.id.planet_statue_name))
+                .setText(resources.getIdentifier("aarde_statue_name", "string", packageName));
+        ((TextView) view.findViewById(R.id.planet_statue_description))
+                .setText(resources.getIdentifier("aarde_statue_description", "string", packageName));
+
+        return view;
     }
 }
--- app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_weetjesFragment.java
+++ app/src/main/java/com/sterrenwacht/cozmix/planeetAarde_weetjesFragment.java
@@ -1,5 +1,6 @@
 package com.sterrenwacht.cozmix;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class planeetAarde_weetjesFragment extends Fragment {
 
@@ -14,10 +16,26 @@ public class planeetAarde_weetjesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_aarde_weetjes, null);
 
+        // inflate fragment view
+        View view = inflater.inflate(R.layout.fragment_aarde_weetjes, null);
 
+        // prep resource management
+        Resources resources = getResources();
+        String packageName = getActivity().getPackageName();
 
+        // set text resources
+        ((TextView)view.findViewById(R.id.planet_moons))
+                .setText(resources.getIdentifier("aarde_moons", "string", packageName));
+        ((TextView)view.findViewById(R.id.planet_temperature))
+                .setText(resources.getIdentifier("aarde_temperature", "string", packageName));
+        ((TextView)view.findViewById(R.id.planet_length_day))
+                .setText(resources.getIdentifier("aarde_length_day", "string", packageName));
+        ((TextView)view.findViewById(R.id.planet_orbital_period))
+                .setText(resources.getIdentifier("aarde_orbital_period", "string", packageName));
+        ((TextView)view.findViewById(R.id.planet_travel_time_sun))
+                .setText(resources.getIdentifier("aarde_travel_time_sun", "string", packageName));
 
+        return view;
     }
 }
